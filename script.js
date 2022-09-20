2b2f5b58da0f561000836ed40e2c928f8cbc41e4
MoisesSRJR <moises.sampaiojr@outlook.com>
2022-09-20 18:48:41 -0300

    adjust class and id

--- script.js
+++ script.js
@@ -1,155 +1,140 @@
 //Função que será adicionada em todos os elementos da classe .btn
-document.querySelectorAll('.btn').forEach((botao) => {
-    document.getElementById(botao.id).addEventListener('click', () => {
-        votacao(botao);
+document.querySelectorAll('.btn').forEach((button) => {
+    document.getElementById(button.id).addEventListener('click', () => {
+        voting (button);
     });
 });
 
 let num1 = document.getElementById('num1');
 let num2 = document.getElementById('num2');
-let somNumeros = document.getElementById('somNumeros');
-let somConfirma = document.getElementById('somConfirma');
+let sondNumber = document.getElementById('sondNumber');
+let sondConfirm = document.getElementById('sondConfirm');
 
-function votacao(botao) {
+function voting (button) {
     if (!num1.value) {
-        num1.innerHTML = botao.value;
-        num1.value = botao.value;
-        somNumeros.play();
+        num1.innerHTML = button.value;
+        num1.value = button.value;
+        sondNumber.play();
         return
     }
     else if (!num2.value) {
-        num2.innerHTML = botao.value;
-        num2.value = botao.value;
-        somNumeros.play();
-        mostraCandidato();
+        num2.innerHTML = button.value;
+        num2.value = button.value;
+        sondNumber.play();
+        showCandidate();
     }
 }
 
-let nomeTitulo = document.getElementById('nomeTitle');
-let nomeCandidato = document.getElementById('nomeCandidato');
-let partido = document.getElementById('partidoCandidato');
-let foto = document.getElementById('fotoPresidente');
-let legenda = document.getElementById('legenda');
+let nameTitle = document.getElementById('nameTitle');
+let nameCandidate = document.getElementById('nameCandidate');
+let broken = document.getElementById('brokenCandidate');
+let img = document.getElementById('imgPresident');
+let legend = document.getElementById('legend');
 
-function mostraCandidato() {
+function showCandidate() {
     if (num1.value == '1' && num2.value == '3') {
-        nomeTitulo.innerText = candidato.lula.nome;
-        nomeCandidato.innerHTML = `Nome: <b>${candidato.lula.nomeCompleto}</b>`;
-        partido.innerHTML = `Partido: <b>${candidato.lula.partido}</b>`;
-        foto.style.backgroundImage = candidato.lula.foto;
-        legenda.style.display = 'flex';
+        nameTitle.innerText = candidato.lula.name;
+        nameCandidate.innerHTML = `name: <b>${candidato.lula.nameComplete}</b>`;
+        broken.innerHTML = `Partido: <b>${candidato.lula.broken}</b>`;
+        img.style.backgroundImage = candidato.lula.img;
+        legend.style.display = 'flex';
     }
 
     else if (num1.value == '2' && num2.value == '2') {
-        nomeTitulo.innerText = candidato.bolsonaro.nome;
-        nomeCandidato.innerHTML = `Nome: <b>${candidato.bolsonaro.nomeCompleto}</b>`;
-        partido.innerHTML = `Partido: <b>${candidato.bolsonaro.partido}</b>`;
-        foto.style.backgroundImage = candidato.bolsonaro.foto;
-        legenda.style.display = 'flex';
+        nameTitle.innerText = candidato.bolsonaro.name;
+        nameCandidate.innerHTML = `name: <b>${candidato.bolsonaro.nameComplete}</b>`;
+        broken.innerHTML = `Partido: <b>${candidato.bolsonaro.broken}</b>`;
+        img.style.backgroundImage = candidato.bolsonaro.img;
+        legend.style.display = 'flex';
     }
 
     else if (num1.value == '1' && num2.value == '2') {
-        nomeTitulo.innerText = candidato.ciro.nome;
-        nomeCandidato.innerHTML = `Nome: <b>${candidato.ciro.nomeCompleto}</b>`;
-        partido.innerHTML = `Partido: <b>${candidato.ciro.partido}</b>`;
-        foto.style.backgroundImage = candidato.ciro.foto;
-        legenda.style.display = 'flex';
-    }
-
-    else if (num1.value == '1' && num2.value == '5') {
-        nomeTitulo.innerText = candidato.moro.nome;
-        nomeCandidato.innerHTML = `Nome: <b>${candidato.moro.nomeCompleto}</b>`;
-        partido.innerHTML = `Partido: <b>${candidato.moro.partido}</b>`;
-        foto.style.backgroundImage = candidato.moro.foto;
-        legenda.style.display = 'flex';
+        nameTitle.innerText = candidato.ciro.name;
+        nameCandidate.innerHTML = `name: <b>${candidato.ciro.nameComplete}</b>`;
+        broken.innerHTML = `Partido: <b>${candidato.ciro.broken}</b>`;
+        img.style.backgroundImage = candidato.ciro.img;
+        legend.style.display = 'flex';
     }
 
     else {
-        nomeTitulo.innerText = 'INVALIDO';
-        nomeCandidato.innerText = 'INVALIDO';
-        partido.innerText = 'INVALIDO';
-        foto.style.backgroundImage = "url('./images/person-icon.png')";
-        legenda.style.display = 'none';
+        nameTitle.innerText = 'INVALIDO';
+        nameCandidate.innerText = 'INVALIDO';
+        broken.innerText = 'INVALIDO';
+        img.style.backgroundImage = "url('./images/person-icon.png')";
+        legend.style.display = 'none';
         num1.value = null;
         num2.value = null;
     }
 }
 
-document.getElementById('btnConfirma').addEventListener('click', confirma);
+document.getElementById('btnConfirm').addEventListener('click', confirmVote);
 
-function confirma() {
+function confirmVote() {
 
-    if (document.getElementById('fim').style.display == 'flex') {
-        corrige();
+    if (document.getElementById('end').style.display == 'flex') {
+        correct();
     } else {
 
         if (num1.value != null && num2.value != null) {
-            document.getElementById('fim').style.display = 'flex';
-            somConfirma.play()
+            document.getElementById('end').style.display = 'flex';
+            sondConfirm.play()
         } else {
             console.log('INVALIDO')
         }
     }
 }
 
-document.getElementById('btnCorrige').addEventListener('click', corrige);
+document.getElementById('btnReplace').addEventListener('click', correct);
 
-function corrige() {
-    nomeTitulo.innerText = 'CANDIDATO';
-    nomeCandidato.innerText = 'Nome:';
-    partido.innerText = 'Partido:';
+function correct() {
+    nameTitle.innerText = 'CANDIDATO';
+    nameCandidate.innerText = 'Nome:';
+    broken.innerText = 'Partido:';
     num1.innerText = '';
     num2.innerText = '';
     num1.value = null;
     num2.value = null;
-    somNumeros.play();
-    foto.style.backgroundImage = "url('./images/person-icon.png')";
-    legenda.style.display = 'none';
-    document.getElementById('fim').style.display = 'none';
+    sondNumber.play();
+    img.style.backgroundImage = "url('./images/person-icon.png')";
+    legend.style.display = 'none';
+    document.getElementById('end').style.display = 'none';
 }
 
-document.getElementById('btnBranco').addEventListener('click', votoBranco);
+document.getElementById('btnWhite').addEventListener('click', voteWhite);
 
-function votoBranco() {
-    nomeTitulo.innerText = 'VOTAR EM BRANCO';
-    nomeCandidato.innerText = '';
-    partido.innerText = '';
+function voteWhite() {
+    nameTitle.innerText = 'VOTAR EM BRANCO';
+    nameCandidate.innerText = '';
+    broken.innerText = '';
     num1.innerText = '0';
     num2.innerText = '0';
     num1.value = '0';
     num2.value = '0';
-    somNumeros.play();
-    foto.style.backgroundImage = "url('./images/person-icon.png')";
-    legenda.style.display = 'none';
+    sondNumber.play();
+    img.style.backgroundImage = "url('./images/person-icon.png')";
+    legend.style.display = 'none';
 }
 
 let candidato = {
 
     lula: {
-        nome: 'LULA',
-        nomeCompleto: 'LUIS INÁCIO LULA DA SILVA',
-        partido: 'PT',
-        foto: "url('./images/lula.jpg')",
+        name: 'LULA',
+        nameComplete: 'LUIS INÁCIO LULA DA SILVA',
+        broken: 'PT',
+        img: "url('./images/lula.jpg')",
     },
 
     bolsonaro: {
-        nome: 'BOLSONARO',
-        nomeCompleto: 'JAIR MESSIAS BOLSONARO',
-        partido: 'PSL',
-        foto: "url('./images/bolsonaro.jpg')",
+        name: 'BOLSONARO',
+        nameComplete: 'JAIR MESSIAS BOLSONARO',
+        broken: 'PSL',
+        img: "url('./images/bolsonaro.jpg')",
     },
 
     ciro: {
-        nome: 'CIRO GOMES',
-        nomeCompleto: 'CIRO FERREIRA GOMES',
-        partido: 'PDT',
-        foto: "url('./images/ciro.jpg')",
-    },
-
-    moro: {
-        nome: 'SERGIO MORO',
-        nomeCompleto: 'SERGIO FERNANDO MORO',
-        partido: 'NOVO',
-        foto: "url('./images/moro.jpg')",
+        name: 'CIRO GOMES',
+        nameComplete: 'CIRO FERREIRA GOMES',
+        broken: 'PDT',
+        img: "url('./images/ciro.jpg')",
     },
 }
